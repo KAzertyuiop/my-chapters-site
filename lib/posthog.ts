70fbbea8b71fe89c13771ce98e3c74307f356cdb
Koen Claes <azertyuiop@MacBook-Pro-2-Azerty.local>
2026-01-01 23:56:34 +0100

    PustHog debugging test in true

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined') {
     process.env.NEXT_PUBLIC_POSTHOG_KEY!,
     {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      capture_pageview: false, // important: manual control
+      capture_pageview: true, // important: manual control
     }
   )
 }
